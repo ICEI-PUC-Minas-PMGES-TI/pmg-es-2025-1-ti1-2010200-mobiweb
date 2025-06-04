58fad9f9111d9a48de8c76381969c65e280636b5
Giovana Lott <giovanalott24@gmail.com>
2025-06-04 16:42:33 -0300

    profissionais ok

--- giovana.lott/script.js
+++ giovana.lott/script.js
@@ -1,39 +1,18 @@
-function abrirModal(nome, especialidade, instagram, link, descricao, local, email, telefone, horario_atendimento, data_nascimento) {
+function abrirModal(nome, especialidade, instagram, linkInstagram, descricao, local, email, telefone, horario, nascimento) {
   document.getElementById("modal-nome").textContent = nome;
   document.getElementById("modal-especialidade").textContent = especialidade;
   document.getElementById("modal-instagram").textContent = instagram;
-  document.getElementById("modal-instagram").href = link;
+  document.getElementById("modal-insta-link").href = linkInstagram;
   document.getElementById("modal-descricao").textContent = descricao;
   document.getElementById("modal-local").textContent = local;
   document.getElementById("modal-email").textContent = email;
   document.getElementById("modal-telefone").textContent = telefone;
-  document.getElementById("modal-horario-atendimento").textContent = horario_atendimento;
-  document.getElementById("modal-data-nascimento").textContent = data_nascimento;
-  document.getElementById("modal").style.display = "flex";
+  document.getElementById("modal-horario").textContent = horario;
+  document.getElementById("modal-nascimento").textContent = nascimento;
+
+  document.getElementById("modal").style.display = "block";
 }
 
 function fecharModal() {
   document.getElementById("modal").style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target === document.getElementById("modal")) {
-    fecharModal();
-  }
-};
-
-function filtrarCards() {
-  const input = document.getElementById("searchInput").value.toLowerCase();
-  const cards = document.getElementsByClassName("card");
-
-  for (let i = 0; i < cards.length; i++) {
-    const nome = cards[i].querySelector("h3").textContent.toLowerCase();
-    const especialidade = cards[i].querySelector("p")?.textContent.toLowerCase() || "";
-
-    if (nome.includes(input) || especialidade.includes(input)) {
-      cards[i].style.display = "block";
-    } else {
-      cards[i].style.display = "none";
-    }
-  }
 }
